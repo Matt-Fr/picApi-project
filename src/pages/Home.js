df336b9f4d6af48c33cff58b053268b3af1422ef
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-06-20 19:33:05 +0200

    added query

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,15 +9,34 @@ const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}${clientID}`;
+    const urlQuery = `&query=${query}`;
+    const urlPage = `&page=${page}`;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
+
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos(data);
+      console.log(data);
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhotos, data.results[0]];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+      });
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -27,10 +46,19 @@ const Home = () => {
 
   useEffect(() => {
     fetchImages();
-  }, []);
+  }, [page]);
 
   useEffect(() => {
-    const event = window.addEventListener("scroll", () => {});
+    const event = window.addEventListener("scroll", () => {
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
     return () => {
       window.removeEventListener("scroll", event);
     };
@@ -38,13 +66,23 @@ const Home = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    fetchImages();
   };
+
+  console.log(query);
 
   return (
     <>
       <section>
         <form>
-          <input type="text" placeholder="search" />
+          <input
+            type="text"
+            placeholder="search"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          />
           <button type="submit" onClick={handleSubmit}>
             <FaSearch></FaSearch>
           </button>
@@ -53,8 +91,12 @@ const Home = () => {
       <section>
         <div>
           {photos.map((image) => {
-            console.log(image);
-            return <Photo key={image.id} {...image}></Photo>;
+            return (
+              <Photo
+                // key={image.id}
+                {...image}
+              ></Photo>
+            );
           })}
         </div>
         {loading && <h2>Loading...</h2>}
